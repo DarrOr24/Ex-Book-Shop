3b2d04114fd7f0cd59166dadd794435f22321614
Darr Or <darrmorgan@gmail.com>
2024-02-04 18:07:19 -0500

    Shift render to to the controller

--- js/book.controller.js
+++ js/book.controller.js
@@ -4,6 +4,29 @@ function onInit(){
     renderBooks()
 }
 
+function renderBooks(){
+    const elTable = document.querySelector('table')
+    const books = getBooks()
+
+    const strHtmls = books.map(book => `<tr class="header">
+        <td class="col1">Title</td>
+        <td class="col2">Price</td>
+        <td class="col3">Actions</td>
+    </tr>
+    <tr>
+        <td class="col1">${book.title}</td>
+        <td class="col2">${book.price}</td>
+        <td class="btns">
+            <button class="read"   onclick="onReadBook('${book.id}')">Read</button>
+            <button class="update" onclick="onUpdateBook('${book.id}')">Update</button>
+            <button class="delete" onclick="onRemoveBook('${book.id}')">Delete</button>
+        </td>
+    </tr>`)
+
+    elTable.innerHTML = strHtmls.join('')
+
+}
+
 function onRemoveBook(bookId){
     removeBook(bookId)
     renderBooks()
@@ -20,4 +43,23 @@ function onAddBook(){
     const bookPrice = prompt('Enter book price')
     addBook(bookTitle, bookPrice)
     renderBooks()
+}
+
+function onReadBook(bookId) {
+    const elModal = document.querySelector('.book-details')
+    const elTxt = elModal.querySelector('h2 span')
+    const elPre = elModal.querySelector('pre')
+
+    const book = readBook(bookId)
+    const bookStr = JSON.stringify(book, null, 4)
+    
+    elTxt.innerText = book.title
+    elPre.innerText = bookStr
+
+    elModal.classList.remove('hidden')
+}
+
+function onCloseBookDetails(ev, elModal) {
+    ev.preventDefault()
+    elModal.classList.add('hidden')
 }
