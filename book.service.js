f436cfdccceeae94de7646899a6d31aafc08d4b6
Darr Or <darrmorgan@gmail.com>
2024-02-04 09:49:32 -0500

    Init repository

---
+++ book.service.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var gBooks = [
+    {id: 'b101', title: 'The Adventures of Lori Ipsi', price: 120},
+    {id: 'b102', title: 'World Atlas', price: 300},
+    {id: 'b103', title: 'Zorba the Greek', price: 87}
+]
+
+function getBooks(){
+    return gBooks
+}
