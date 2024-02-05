86b653f2e57786ef8aca25dbeabf337766fd3f7f
Darr Or <darrmorgan@gmail.com>
2024-02-04 19:02:36 -0500

    Add support for local storage

--- js/book.service.js
+++ js/book.service.js
@@ -1,10 +1,13 @@
 'use strict'
 
-var gBooks = [
-    {id: 'b101', title: 'The Adventures of Tom Sawyer', price: 120, img: 'img/The-Adventures-of-Tom-Sawyer.png'},
-    {id: 'b102', title: 'World Atlas', price: 300, img: 'img/World-Atlas.png'},
-    {id: 'b103', title: 'Zorba the Greek', price: 87, img: 'img/Zorba-the-Greek.png'}
-]
+// var gBooks = [
+//     {id: 'b101', title: 'The Adventures of Tom Sawyer', price: 120, img: 'img/The-Adventures-of-Tom-Sawyer.png'},
+//     {id: 'b102', title: 'World Atlas', price: 300, img: 'img/World-Atlas.png'},
+//     {id: 'b103', title: 'Zorba the Greek', price: 87, img: 'img/Zorba-the-Greek.png'}
+// ]
+
+var gBooks
+_createBooks()
 
 function getBooks(){
     return gBooks
@@ -15,31 +18,58 @@ function removeBook(bookId){
     const idx = gBooks.findIndex(book => book.id === bookId)
     gBooks.splice(idx,1)
 
+    _saveBooks()
+
 }
 
 function updatePrice(bookId, newPrice){
     const book = gBooks.find(book => book.id === bookId)
     book.price = newPrice
-    // return book
+    _saveBooks()
+    return book
 }
 
 function addBook(bookTitle, bookPrice){
     const book = _createBook(bookTitle, bookPrice)
     gBooks.unshift(book)
-    // return book
+    _saveBooks()
+    return book
 
-}
-
-function _createBook(title, price){
-    return {
-        id: makeId(),
-        title,
-        price,
-    }
 }
 
 function readBook(bookId) { // Read
     const book = gBooks.find(book => book.id === bookId)
     return book
 }
+
+
+
+//Private functions
+
+function _createBooks() {
+    gBooks = loadFromStorage('bookDB')
+    if(!gBooks || gBooks.length === 0){
+
+        gBooks = [
+            _createBook('The Adventures of Tom Sawyer', 120, 'img/The-Adventures-of-Tom-Sawyer.png'), 
+            _createBook('World Atlas', 300, 'img/World-Atlas.png'), 
+            _createBook('Zorba the Greek', 87, 'img/Zorba-the-Greek.png')
+        ]
+        _saveBooks()
+    }
+}
+
+function _createBook(title, price, img = 'img/book-cover.png'){
+    return {
+        id: makeId(),
+        title,
+        price,
+        img,
+    }
+}
+
+function _saveBooks() {
+    saveToStorage('bookDB', gBooks)
+}
+
 
